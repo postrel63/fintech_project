6a14ea001da85e3694bdc95e97024f6250d3a48f
postrel63 <postrel63@naver.com>
2023-08-25 22:15:09 +0900

    docker 설정, 메모

--- docker/fintech-redis.sh
+++ docker/fintech-redis.sh
@@ -4,3 +4,6 @@ docker run --name fintech-redis \
              -p 6379:6379 \
              --network redis-net \
              -d redis:latest
+
+#redis-cli -h localhost -p 6379
+#localhost의 6379포트에 실행중인 redis 서버에 접속해라
